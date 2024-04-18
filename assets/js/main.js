221707fbe9aa3a6a338e0f9ba9ab52a385065118
Naiem-Devs <dnsnaiem@gmail.com>
2024-04-19 01:21:30 +0600

    complate

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,41 +6,19 @@
     event.preventDefault()
     $(this).toggleClass('active');   
     $('.mobile-menu').toggleClass('siteBar');   
+    $('body').toggleClass('overflow-hidden');   
   }); 
 
 
-  // owlCarousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
-    nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
-    }
-  });
 
 
   // page Animation
-  // AOS.init({
-  //   mirror: true,
-  //   duration: 1500,
-  //   initClassName: 'aos-init',
-  //   once: true,
-  // });
+  AOS.init({
+    mirror: true,
+    duration: 1500,
+    initClassName: 'aos-init',
+    once: true,
+  });
 
   // data-aos="fade-up" 
   // data-aos-delay="300" 
